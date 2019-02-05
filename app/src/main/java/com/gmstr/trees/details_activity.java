06d83bcc70a715fabebf57f957bb2db3d31c5822
szekelyg <szekelygellert@inf.elte.hu>
2019-02-05 21:42:26 +0200

    Initial commit

---
+++ app/src/main/java/com/gmstr/trees/details_activity.java
@@ -0,0 +1,13 @@
+package com.gmstr.trees;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class details_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_activity);
+    }
+}
