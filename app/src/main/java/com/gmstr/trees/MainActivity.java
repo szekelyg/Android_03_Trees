9188e655644ce0a8eff1c10bf1784c4a44b1134f
szekelyg <szekelygellert@inf.elte.hu>
2019-02-05 23:27:44 +0200

    Finish project.

--- app/src/main/java/com/gmstr/trees/MainActivity.java
+++ app/src/main/java/com/gmstr/trees/MainActivity.java
@@ -1,13 +1,51 @@
 package com.gmstr.trees;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView treeOne;
+    private ImageView treeTwo;
+
+    private String bukkBio = "A hűvösebb és csapadékosabb éghajlatot kedvelő, mészkedvelő faj, ezért Magyarországon leginkább a középhegységek tetőin, illetve északi lejtőin nő, az Alföld peremére ritkán ereszkedik le. ";
+    private String tolgyBio = "Az egyes fajok termete meglehetősen változatos: cserjék és nagy (mintegy 35 m magasságig) fák egyaránt előfordulnak a nemzetségben. Kérge repedezett."
+            +" A lombhullató fajok levele többnyire öblös, karéjos, a mediterrán fajoké rendszerint tagolatlan.";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        treeOne = findViewById(R.id.Bukk_ID);
+        treeTwo = findViewById(R.id.Tolgy_ID);
+
+        treeOne.setOnClickListener(this);
+        treeTwo.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.Bukk_ID:
+                Intent bukkIntent = new Intent(MainActivity.this,details_activity.class);
+                bukkIntent.putExtra("bukk",bukkBio);
+                bukkIntent.putExtra("name","bukk");
+                startActivity(bukkIntent);
+                break;
+            case R.id.Tolgy_ID:
+                Intent tolgyIntent = new Intent(MainActivity.this,details_activity.class);
+                tolgyIntent.putExtra("tolgy",tolgyBio);
+                tolgyIntent.putExtra("name","tolgy");
+                startActivity(tolgyIntent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/gmstr/trees/details_activity.java
+++ app/src/main/java/com/gmstr/trees/details_activity.java
@@ -2,12 +2,51 @@ package com.gmstr.trees;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
 public class details_activity extends AppCompatActivity {
+
+    private TextView bioText;
+    private ImageView profileImage;
+    private Bundle extras;
+    private TextView treeName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_activity);
+
+        profileImage = findViewById(R.id.Image_ID);
+        bioText = findViewById(R.id.textView_ID);
+        treeName=findViewById(R.id.nameID);
+
+        extras = getIntent().getExtras();
+
+        bioText.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+
+        if (extras!=null){
+
+            String name = extras.getString("name");
+            showDetails(name);
+
+            //Toast.makeText(this, bio, Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    public void showDetails(String name){
+        if(name.equals("bukk")){
+            profileImage.setImageDrawable(getResources().getDrawable(R.drawable.bukk));
+            treeName.setText("Bükk");
+            bioText.setText(extras.getString(name));
+        }else if(name.equals("tolgy")){
+            profileImage.setImageDrawable(getResources().getDrawable(R.drawable.tolgy));
+            treeName.setText("Tölgy");
+            bioText.setText(extras.getString(name));
+        }
     }
 }
